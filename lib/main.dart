9e1480bc593d95b15418e904c5b27f01cb9d8078
Dafei Ning <ningdafei@gmail.com>
2019-10-27 18:55:01 -0700

    add a bar and title

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   // The Build() method returns the widgets(widgest tree) which should be rendered onto the screen.
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('角色行为判定'),
+          ),
+          body: Text('Implementation of character judge')),
+    );
   }
 }
