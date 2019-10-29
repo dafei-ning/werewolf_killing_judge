74cf6dbe0c8547e29100b4c6cb4dab2afcb09262
Dafei Ning <ningdafei@gmail.com>
2019-10-29 02:15:17 -0700

    Question's postion and margin

--- lib/question.dart
+++ lib/question.dart
@@ -9,6 +9,15 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 25),
+        textAlign: TextAlign.center,
+      ),
+      width: double.infinity,
+      height: 50,
+      margin: EdgeInsets.all(25),
+    );
   }
 }
